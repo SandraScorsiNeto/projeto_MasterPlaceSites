f20870d2651a8de29f89d5068bd032da927eceae
Sandra Scorsi Neto <sandrascorsi3@hotmail.com>
2025-05-26 20:18:22 -0300

    alterando page

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,9 +1,10 @@
 // lib/supabaseClient.ts
-import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+//import { createClient } from '@supabase/supabase-js'
 
-export default supabase
+//const supabase = createClient(
+//  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+//)
+
+//export default supabase;
